7bc899f5fc16385288dbf4c6f93609f586367742
Jubayer-jm <mdjubayerahmed762@gmail.com>
2026-04-23 20:49:57 +0600

    ok

---
+++ Product1.java
@@ -0,0 +1,19 @@
+package BasicJava;
+
+public class Product1 {
+    public static void main(String[]args){
+        int id = 101;
+        String title = "iphone15";
+        String price = "1895 euros";
+        String description = "Perfect productct";
+        String category = "good";
+        System.out.println(" id : "+ id);
+        System.out.println("title: "+ title);
+        System.out.println("price: "+ price);
+        System.out.print("description: "+ description);
+        System.out.println("category: "+category);
+
+    }
+
+
+}
